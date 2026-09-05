9e23e16218b02978822571ae6a869781dbb27e9b
heidi <h.nyula@gmail.com>
2026-09-05 02:20:21 +0200

    add some basic db constrains

--- db/migrate/20260905001459_create_tracker_rooms.rb
+++ db/migrate/20260905001459_create_tracker_rooms.rb
@@ -1,7 +1,7 @@
 class CreateTrackerRooms < ActiveRecord::Migration[8.1]
   def change
     create_table :tracker_rooms do |t|
-      t.string :slug
+      t.string :slug, null: false
 
       t.timestamps
     end
--- db/migrate/20260905001540_create_tracker_items.rb
+++ db/migrate/20260905001540_create_tracker_items.rb
@@ -2,11 +2,13 @@ class CreateTrackerItems < ActiveRecord::Migration[8.1]
   def change
     create_table :tracker_items do |t|
       t.references :tracker_room, null: false, foreign_key: true
-      t.integer :position
-      t.string :image_name
-      t.integer :value
+      t.integer :position, null: false
+      t.string :image_name, null: false
+      t.integer :value, null: false, default: 0
 
       t.timestamps
     end
+
+    add_index :tracker_items, [ :tracker_room_id, :position ], unique: true
   end
 end
