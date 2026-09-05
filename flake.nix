13d22b2bdcae9dfa1fa11cdc073ebaf85fffeffc
heidi <h.nyula@gmail.com>
2026-09-05 20:50:00 +0200

    working nix

--- flake.nix
+++ flake.nix
@@ -1,89 +1,73 @@
 {
+  description = "A very basic flake";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
     services-flake.url = "github:juspay/services-flake";
   };
 
-  outputs = inputs@{
-    self,
-    nixpkgs,
-    flake-parts,
-    process-compose-flake,
-    services-flake,
-    ...
-  }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
+  outputs = inputs@{ flake-parts, ... }:
+  # https://flake.parts/module-arguments.html
+  flake-parts.lib.mkFlake { inherit inputs; } (top@{ config, withSystem, moduleWithSystem, ... }: {
+    imports = [
+      # Optional: use external flake logic, e.g.
+      # inputs.foo.flakeModules.default
+      inputs.process-compose-flake.flakeModule
+    ];
+    flake = {
+      # Put your original flake attributes here.
+    };
+    systems = [
+      # systems for which you want to build the `perSystem` attributes
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+      # ...
+    ];
+    perSystem = { config, pkgs, ... }: {
+      # Recommended: move all package definitions here.
+      # e.g. (assuming you have a nixpkgs input)
+      # packages.foo = pkgs.callPackage ./foo/package.nix { };
+      # packages.bar = pkgs.callPackage ./bar/package.nix {
+      #   foo = config.packages.foo;
+      # };
+      devShells.default = let dev = pkgs.writeShellScriptBin "dev" 
+      ''
+        nix run
+      ''; 
+      in pkgs.mkShell {
+        buildInputs = [ pkgs.ruby pkgs.libyaml pkgs.watchman dev ];
+      };
 
-      imports = [
-        process-compose-flake.flakeModule
-      ];
+      process-compose.default = {
+        imports = [
+          inputs.services-flake.processComposeModules.default
+        ];
+        settings.processes = {
+          setup.command = "bin/setup --skip-server";
+          setup.depends_on.pg.condition = "process_healthy";
 
-      perSystem = { pkgs, ... }: {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            ruby
-            libyaml
-            postgresql_16
-          ];
-
-          PGHOST = "127.0.0.1";
-          PGPORT = "8286";
-          DATABASE_URL = "postgresql://127.0.0.1:8286/tatracker_development";
-          RAILS_ENV = "development";
+          server.command = "bin/dev";
+          server.depends_on.setup.condition = "process_completed";
         };
 
-        process-compose.default = {
-          imports = [
-            services-flake.processComposeModules.default
-          ];
+        services.postgres."pg" = {
+          enable = true;
+          package = pkgs.postgresql_16;
 
-          services.postgres."pg" = {
-            enable = true;
-            package = pkgs.postgresql_16;
+          port = 8286;
+          # Postgres accepts hostnames/IPs here; using 127.0.0.1 avoids ipv6/localhost weirdness. :contentReference[oaicite:1]{index=1}
+          listen_addresses = "127.0.0.1";
 
-            listen_addresses = "127.0.0.1";
-            port = 8286;
-
-            initialDatabases = [
-              { name = "tatracker_development"; }
-              { name = "tatracker_test"; }
-            ];
-          };
-
-          settings.processes = {
-            setup = {
-              command = "bin/rails db:prepare";
-
-              depends_on.pg.condition = "process_healthy";
-
-              availability = {
-                restart = "no";
-              };
-            };
-
-            server = {
-              command = "bin/rails server";
-
-              depends_on.setup.condition = "process_completed_successfully";
-            };
-          };
-        };
-
-        packages.default = self.packages.${pkgs.system}.dev;
-
-        packages.dev = process-compose-flake.lib.mkProcessCompose {
-          inherit pkgs;
-          modules = [
-            self.process-compose.${pkgs.system}.default
+          initialDatabases = [
+            { name = "tatracker_development"; }
+            { name = "tatracker_test"; }
           ];
         };
       };
     };
+  });
+
 }
